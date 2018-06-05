80f1c9fc247c672a42ef33d4e011da6e10fbf01d
wiximix <wiximix@gmail.com>
2018-06-05 17:32:04 +0800

    createjs combined

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import 'latest-createjs'
 
-import * as clazz from './class/'
-import * as utils from './utils'
+import * as clazz from './src/class/'
+import * as utils from './src/utils'
 
 if (!window.babyEye) {
   window.babyEye = {}
   Object.assign(window.babyEye, clazz, utils)
 }
 
-export * from './class/'
-export * from './utils'
+export * from './src/class/'
+export * from './src/utils'
 export default babyEye
 
---
+++ src/utils/vison.js
@@ -0,0 +1,11 @@
+/**
+ * PD * distance/(PD + disparty)
+ * @param {Number} disparty 视差
+ * @param {Number} distance 距离
+ * @param {Number} PD 瞳距
+ * @param {mode} mode 瞳距
+ * 参数单位皆为毫米
+ */
+export function vergence(disparty = 0, distance = 400, PD = 60, mode = 'RAD' ) {
+    return
+}

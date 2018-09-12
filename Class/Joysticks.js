8a72207e1b1b97c2b0bfde98979917d507d598a5
wiximix <wiximix@gmail.com>
2018-09-12 14:00:32 +0800

    rm 小写 physics

--- Class/Joysticks.js
+++ Class/Joysticks.js
@@ -1,119 +1,120 @@
 class Joystick {
-    constructor(index) {
-        this.index = index
-        this.pressedMap = {}
-        this.oldPressedMap = {}
-        this.deadZone = 0.1
-        this.setMap()
+  constructor(index) {
+    this.index = index
+    this.pressedMap = {}
+    this.oldPressedMap = {}
+    this.deadZone = 0.1
+    this.setMap()
+  }
+
+  update() {
+    this.oldPressedMap = this.pressedMap
+    this.pressedMap = {}
+
+    let pad = navigator.getGamepads()[this.index]
+    if (!pad) return
+    //buttons
+    pad.buttons.forEach((button, index) => {
+      let key = this.defaultMap.buttons[index]
+      this.pressedMap[key] = button.pressed
+    })
+
+    //axies
+    pad.axes.forEach((value, index) => {
+      let key = this.defaultMap.axes[index]
+      if (Math.abs(value) < this.deadZone) {
+        value = 0
+      }
+      this.pressedMap[key] = value
+    })
+  }
+
+  justPressed(key) {
+    return this.pressedMap[key] && !this.oldPressedMap[key]
+  }
+
+  pressed(key) {
+    return this.pressedMap[key]
+  }
+
+  justRelesed(key) {
+    return !this.pressedMap[key] && this.oldPressedMap[key]
+  }
+
+  setMap() {
+    this.defaultMap = {
+      buttons: {
+        0: 'A',
+        1: 'B',
+        2: 'X',
+        3: 'Y',
+        4: 'LB',
+        5: 'RB',
+        6: 'LT',
+        7: 'RT',
+        8: 'BACK',
+        9: 'START',
+        10: 'LSTICK',
+        11: 'RSTICK',
+        12: 'UP',
+        13: 'DOWN',
+        14: 'LEFT',
+        15: 'RIGHT',
+        16: 'SWITCH'
+      },
+      axes: {
+        0: 'ALX',
+        1: 'ALY',
+        2: 'ARX',
+        3: 'ARY'
+      }
     }
-
-    update(){
-        this.oldPressedMap = this.pressedMap
-        this.pressedMap = {}
-
-        let pad = navigator.getGamepads()[this.index]
-        if(!pad) return
-        //buttons
-        pad.buttons.forEach((button, index)=>{
-            let key = this.defaultMap.buttons[index]
-            this.pressedMap[key] = button.pressed
-        })
-
-        //axies
-        pad.axes.forEach((value,index)=>{
-            let key = this.defaultMap.axes[index]
-            if(Math.abs(value) < this.deadZone) {
-                value = 0
-            }
-            this.pressedMap[key] = value
-        })
-    }
-
-    justPressed(key){
-        return this.pressedMap[key] && !this.oldPressedMap[key]
-    }
-
-    pressed(key){
-        return this.pressedMap[key]
-    }
-
-    justRelesed(key){
-        return !this.pressedMap[key] && this.oldPressedMap[key]
-    }
-
-    setMap(){
-        this.defaultMap = {
-            buttons:{
-                0: 'A',
-                1: 'B',
-                2: 'X',
-                3: 'Y',
-                4: 'LB',
-                5: 'RB',
-                6: 'LT',
-                7: 'RT',
-                8: 'BACK',
-                9: 'START',
-                10: 'LSTICK',
-                11: 'RSTICK',
-                12: 'UP',
-                13: 'DOWN',
-                14: 'LEFT',
-                15: 'RIGHT',
-                16: 'SWITCH'
-            },
-            axes: {
-                0: 'ALX',
-                1: 'ALY',
-                2: 'ARX',
-                3: 'ARY'
-            }
-        }
-    }
+  }
 }
 
 class Joysticks {
-    constructor(){
-        this.controllers = []
-        for(let i = 0; i < 4; i++){
-            let controller = new Joystick(i)
-            this.controllers.push(controller)
-        }
+  constructor() {
+    this.controllers = []
+    for (let i = 0; i < 4; i++) {
+      let controller = new Joystick(i)
+      this.controllers.push(controller)
     }
+  }
 
-    currentNum(){//当前连接手柄数
-        let gamepads = navigator.getGamepads()
-        let result = 0
-        for(let i = 0; i < 4; i++){
-            if(gamepads[i]){
-                result += 1
-            }
-        }
-        return result
+  currentNum() {
+    //当前连接手柄数
+    let gamepads = navigator.getGamepads()
+    let result = 0
+    for (let i = 0; i < 4; i++) {
+      if (gamepads[i]) {
+        result += 1
+      }
     }
+    return result
+  }
 
-    checkValid(id){
-        let gamepads = navigator.getGamepads()
-        return gamepads[id]
-    }
+  checkValid(id) {
+    let gamepads = navigator.getGamepads()
+    return gamepads[id]
+  }
 
-    update(){
-        this.controllers.forEach((controller)=>{
-            controller.update()
-        })
-    }
+  update() {
+    this.controllers.forEach(controller => {
+      controller.update()
+    })
+  }
 
-    justPressed(key, index=0){
-        return this.controllers[index].justPressed(key)
-    }
+  justPressed(key, index = 0) {
+    return this.controllers[index].justPressed(key)
+  }
 
-    pressed(key, index=0){
-        return this.controllers[index].pressed(key)
-    }
+  pressed(key, index = 0) {
+    return this.controllers[index].pressed(key)
+  }
 
-    justRelesed(key, index=0){
-        return this.controllers[index].justPressed(key)
-    }
+  justRelesed(key, index = 0) {
+    return this.controllers[index].justPressed(key)
+  }
 }
 
 export { Joysticks }
